67d30475986737f241c49f133d0a5cd05d6c69ea
sugar-water <sugarw.ter@gmail.com>
2021-01-06 17:50:03 -0500

    setup main-menu

--- js/index.js
+++ js/index.js
@@ -2,13 +2,20 @@ let grid = document.getElementById("grid");
 let tempCanvas = document.createElement("canvas");
 let tempCxt = tempCanvas.getContext("2d");
 
-const quote = `“Between words and objects one can create new relations and specify characteristics of language and objects generally ignored in everyday life.”
-― René Magritte `.replace(/\s/g, "⚹");
+const quote = "/)"
+
+// JS needs python comprehensions.
+const arrowsMap = {
+    2190: 2192, 2192: 2190,
+    2191: 2193, 2193: 2191,
+    2194: 2194, 2195: 2195,
+    2196: 2198, 2198: 2196,
+    2197: 2199, 2199: 2197,
+};
+const arrows = Object.values(arrowsMap)
 
 const numPages = 14282;
-
-let randomChar = () =>
-    String.fromCharCode(97+Math.floor(Math.random() * 26));
+let randomArrow = () => arrows[Math.floor(Math.random() * (arrows.length- 1))]
 
 let getSquareSideLength = (n) =>
 {
@@ -18,12 +25,12 @@ let getSquareSideLength = (n) =>
     let nSideLength = Math.sqrt(nArea);
     let nWide = Math.round(rect.width / nSideLength);
     let nHigh = Math.round(rect.height / nSideLength);
-    return [nSideLength, nWide, nHigh];
+    return [nSideLength - 0.3, nWide, nHigh];
 }
 
 let populateChars = (len, nWide, nHigh) => 
 {
-    let i = 0;
+    let _i = 0;
     return [...Array(nHigh).keys()].map((nH, i) => {
         return [...Array(nWide).keys()].map((nW, ii) => {
             span = document.createElement("span");
@@ -32,8 +39,8 @@ let populateChars = (len, nWide, nHigh) =>
                 span.innerHTML = `<span class="unit contained">❋</span>`;
             }
             else {
-                span.innerHTML = `<span class="unit contained">${quote[i % quote.length]}</span>`;
-                i++;
+                span.innerHTML = `<span class="unit contained">${quote[_i % quote.length]}</span>`;
+                _i++;
             }
             span.style.width = `${len}px`;
             span.style.height = `${len}px`;
@@ -45,7 +52,8 @@ let populateChars = (len, nWide, nHigh) =>
     });
 }
 
-let centerGrid = (len, nWide, nHigh) => {
+let centerGrid = (len, nWide, nHigh) => 
+{
     let w = len * nWide;
     let h = len * nHigh;
     grid.style.height = `${h}px`;
@@ -96,11 +104,23 @@ let colorTheUnits = (units, pixels) => {
             if (!unit.style.backgroundColor) {
                 unit.style.backgroundColor = `rgb(${r},${g},${b})`;
             }
-            if (((r+g+b)/3) < 100) {
+            if (((r+g+b)/3) < 130) {
                 unit.style.color = "white";
             }
             if (!unit.titleSet && j !== 0) {
-                unit.firstChild.innerText = randomChar();
+                let rArrow = randomArrow();
+                unit.firstChild.innerText = String.fromCharCode("0x" + rArrow);
+                if (Math.random() <= 0.5) {
+                    let oArrow = arrowsMap[rArrow];
+                    unit.originalArrow = unit.firstChild.innerText;
+                    unit.reversedArrow = String.fromCharCode("0x" + oArrow);
+                    unit.addEventListener("mouseover", function() {
+                        this.firstChild.innerText = this.reversedArrow;
+                    })
+                    unit.addEventListener("mouseout", function() {
+                        this.firstChild.innerText = this.originalArrow;
+                    })
+                }
             }
         }, Math.random() * 50000);
     }
@@ -109,11 +129,13 @@ let colorTheUnits = (units, pixels) => {
 const insertTitle = (title, units) => {
     console.log("Title of Artwork:", title);
     let lastRow = units[units.length - 1];
-    let width = units[0].length;
     let betterTitle = title.replace(/\s/g, "⚹");
     lastRow.forEach((u, i) => {
         setTimeout(() => {
             u.titleSet = true;
+            if (u.originalArrow) {
+                u.originalArrow = betterTitle[i];
+            }
             if (i < betterTitle.length) {
                 u.firstChild.innerText = betterTitle[i];
             } else {
@@ -123,18 +145,40 @@ const insertTitle = (title, units) => {
     });
 }
 
-let linkImg = (units) => {
+let linkImg = (units) => 
+{
     linker = units[0][0];
     linker.id = "link-to-image";
     return (url) => { linker.onclick = () => window.open(url); }
 }
 
+let makeMainMenu = (units) => 
+{
+    mm = document.getElementById("main-menu");
+    beginL = 1;
+    endL = units[0].length - 10;
+    beginT = 1;
+    endT = units.length - 10;
+    rtop = Math.floor(Math.random() * (endT - beginT) + beginT);
+    rleft = Math.floor(Math.random() * (endL - beginL) + beginL);
+    let getTL = (top, left) => [units[top][left].style.top, units[top][left].style.left];
+    [top_, left_] = getTL(rtop, rleft);
+    console.log(beginL, endL, units[0].length)
+    console.log(beginT, endT, units.length)
+    mm.style.top = top_;
+    mm.style.left = left_;
+    mm.style.width = parseFloat(units[0][0].style.width) * 10 + "px";
+    mm.style.height = parseFloat(units[0][0].style.height) * 10 + "px";
+    mm.style.display = "block";
+}
+
 let setup = (n) => 
 {
-    let u = getSquareSideLength(n)
+    let u = getSquareSideLength(n);
     let units = populateChars(...u);
-    let linkUrl = linkImg(units);
     centerGrid(...u);
+    makeMainMenu(units);
+    let linkUrl = linkImg(units);
     getRandomImage()
         .then(([title, url]) => {
             linkUrl(url);
@@ -146,7 +190,7 @@ let setup = (n) =>
 
 window.onload = () => {
     if (document.body.clientWidth < 400) { setup(250) }
-    else if (document.body.clientWidth < 600) { setup(300) }
+    else if (document.body.clientWidth < 600) { setup(200) }
     else if (document.body.clientWidth < 800) { setup(500) }
     else if (document.body.clientWidth < 1000) { setup(700) }
     else if (document.body.clientWidth < 1200) { setup(900) }
